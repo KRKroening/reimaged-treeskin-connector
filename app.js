ad5a0fc6b02c133da9d038162e66bb4214d7393f
KimK <KimK@Treeskin.ca>
2018-05-15 22:21:55 -0600

    Added node.js files

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var db = require('./db');
+ 
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
+
+var ProviderController = require('./Provider/providerController');
+var SubjectController = require('./Subject/subjectController');
+
+app.use('/providers', ProviderController)
+app.use('/subjects', SubjectController)
+
+
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://ts_admin:ts_pass@ts-01-penqj.mongodb.net/Treeskin_db')
